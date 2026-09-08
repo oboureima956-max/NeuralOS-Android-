e8a9841e6ae15b6adc37d5ce8c5e3f0a7e5b10b3
Boureima Ouedraogo <oboureima956@gmail.com>
2026-09-08 20:39:08 +0000

    Update settings gradle.kts

--- settings gradle.kts
+++ settings gradle.kts
@@ -1,1 +1,22 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NeuralOS"
+
+include(":app")
